4ee133fad2ae9735be130ca268cb31a76b603aa4
Jorge Guerrero <jorgegp0408@gmail.com>
2023-11-29 13:55:23 +0100

    David

---
+++ David/src/david/holamundo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package david;
+
+/**
+ *
+ * @author FP
+ */
+public class holamundo {
+    public static void main(String[] args) {
+        
+    }
+}
